90f314a1d381b306c55c8d228449a49c05014e3a
s.areffard <saeedareffard@yahoo.com>
2023-09-07 11:58:46 +1200

    fix: change csproj

--- ExpanseTracker/Program.cs
+++ ExpanseTracker/Program.cs
@@ -63,7 +63,7 @@ builder.Services.AddSwaggerGen(c =>
 });
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
-    options.UseSqlServer(b => b.MigrationsAssembly("ExpanseTracker"));
+    options.UseSqlServer(b => b.MigrationsAssembly("ExpanseTrackerAzure"));
     options.UseSqlServer(builder.Configuration.GetConnectionString("ExpanseTrackerAzure"));
 });
 builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
