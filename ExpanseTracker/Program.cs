88c5d82dafc11e7b54a72c476e420cf2aded71c6
s.areffard <saeedareffard@yahoo.com>
2023-09-07 13:22:26 +1200

    add startup.cs

--- ExpanseTracker/Program.cs
+++ ExpanseTracker/Program.cs
@@ -1,90 +1,16 @@
-using System.Text;
-using Application.Services;
-using Domain.Interfaces;
-using Infrastructure.Helpers;
-using Infrastructure.Persistence.contexts;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
+namespace ExpanseTracker;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
-// Add services to the container.
-builder.Services.AddControllers();
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = builder.Configuration["Tokens:Issuer"],
-            ValidAudience = builder.Configuration["Tokens:Issuer"],
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Tokens:Key"]))
-        };
-    });
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
+public class Program
 {
-    c.SwaggerDoc("v1", new OpenApiInfo
+    public static void Main(string[] args)
     {
-        Title = "Expanse Tracker",
-        Version = "v1"
-    });
- /*   c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Description =
-            "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\""
-    });
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
             {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            new string[] { }
-        } 
-    }); */
-});
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ExpanseTrackerAzure"));
-    options.UseSqlServer(b => b.MigrationsAssembly("ExpanseTracker"));
-});
-builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
-builder.Services.AddTransient<GoalService>();
-builder.Services.AddTransient<TransactionService>();
-builder.Services.AddTransient<CategoryService>();
-
-var app = builder.Build();
-app.UseAuthentication();
-// Configure the HTTP request pipeline.
-app.UseSwagger();
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+                webBuilder.UseStartup<Startup>();
+            });
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
---
+++ ExpanseTracker/Startup.cs
@@ -0,0 +1,102 @@
+﻿using System.Text;
+using Application.Services;
+using Domain.Interfaces;
+using Infrastructure.Helpers;
+using Infrastructure.Persistence.contexts;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+namespace ExpanseTracker;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddSingleton(Configuration);
+// Add services to the container.
+        services.AddControllers();
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = Configuration["Tokens:Issuer"],
+                    ValidAudience = Configuration["Tokens:Issuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Tokens:Key"]!))
+                };
+            });
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "Expanse Tracker",
+                Version = "v1"
+            });
+            /*   c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+               {
+                   Name = "Authorization",
+                   Type = SecuritySchemeType.ApiKey,
+                   Scheme = "Bearer",
+                   BearerFormat = "JWT",
+                   In = ParameterLocation.Header,
+                   Description =
+                       "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\""
+               });
+               c.AddSecurityRequirement(new OpenApiSecurityRequirement
+               {
+                   {
+                       new OpenApiSecurityScheme
+                       {
+                           Reference = new OpenApiReference
+                           {
+                               Type = ReferenceType.SecurityScheme,
+                               Id = "Bearer"
+                           }
+                       },
+                       new string[] { }
+                   } 
+               }); */
+        });
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseSqlServer(Configuration.GetConnectionString("ExpanseTrackerAzure"));
+            options.UseSqlServer(b => b.MigrationsAssembly("ExpanseTracker"));
+        });
+        services.AddTransient<IUnitOfWork, UnitOfWork>();
+        services.AddTransient<GoalService>();
+        services.AddTransient<TransactionService>();
+        services.AddTransient<CategoryService>();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        app.UseAuthentication();
+        // Configure the HTTP request pipeline.
+        app.UseSwagger();
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+
+    }
+}
